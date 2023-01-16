426b07b4a26a147c4e4bde9b277073ccd11e5226
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-01-16 11:00:13 +0100

    Static root config

--- backend/urls.py
+++ backend/urls.py
@@ -26,10 +26,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     # path('', TemplateView.as_view(template_name='index.html')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [re_path(r'^.*',
                         TemplateView.as_view(template_name='index.html'))]
-
-urlpatterns = [
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
