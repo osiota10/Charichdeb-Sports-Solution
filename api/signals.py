84684e4a8f25e37fecf34155289598f333e2197f
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-05-10 04:22:34 +0100

    New user notification

--- api/signals.py
+++ api/signals.py
@@ -1,6 +1,13 @@
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
 from .models import *
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 def slug_generator(sender, instance, *args, **kwargs):
@@ -9,4 +16,14 @@ def slug_generator(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(slug_generator, sender=Event)
-# pre_save.connect(slug_generator, sender=OurProject)
+
+
+@receiver(post_save, sender=User)
+def send_new_user_email(sender, instance, created, **kwargs):
+    if created:
+        subject = 'New User Registration'
+        message = render_to_string('new_user_email.html', {'user': instance})
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = ['osiotaobrozie@gmail.com']
+        send_mail(subject, message, from_email, recipient_list, html_message=message)
+
