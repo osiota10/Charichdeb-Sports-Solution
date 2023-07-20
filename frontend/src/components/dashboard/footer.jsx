866da41ef5e619090bdbc491d77d349b4a4c9376
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-20 14:56:58 +0100

    Company name

--- frontend/src/components/dashboard/footer.jsx
+++ frontend/src/components/dashboard/footer.jsx
@@ -1,10 +1,12 @@
-
+import { useContext } from "react";
+import { CompanyInformationContext } from "../../App";
 
 function DashboardFooter() {
+    const companyInfo = useContext(CompanyInformationContext)
     const date = new Date()
     return (
         <footer class="row bg-primary py-2">
-            <p class="text-center text-white">Copyright © Charichdeb! {date.getFullYear()}</p>
+            <p class="text-center text-white"> © {date.getFullYear()} {companyInfo.company_name} </p>
         </footer>
     );
 }
