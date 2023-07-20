57c5b912fde4ed80f71e426ec35d1dc5fc6bcdeb
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-20 18:13:04 +0100

    Code clean up

--- frontend/src/components/dashboard/layout.jsx
+++ frontend/src/components/dashboard/layout.jsx
@@ -1,19 +1,11 @@
 import DashboardSideBar from "./navBar";
 import DashboardFooter from "./footer";
-import { Connect, connect } from "react-redux";
-import { checkAuthenticated } from "../../actions/auth";
-import { useEffect } from "react";
 
-const DashboardLayout = ({ checkAuthenticated }) => {
-    const result = checkAuthenticated()
-    console.log(result)
-    useEffect(() => {
-        checkAuthenticated();
-        // load_user();
-    }, []);
+
+const DashboardLayout = () => {
 
     return (
-        <section className="">
+        <section>
             <DashboardSideBar />
 
             <DashboardFooter />
@@ -21,4 +13,4 @@ const DashboardLayout = ({ checkAuthenticated }) => {
     )
 };
 
-export default connect(null, { checkAuthenticated })(DashboardLayout);
+export default DashboardLayout;
