b11c417a6f434cedc6b481484334ec0e3118cd6f
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-21 11:51:45 +0100

    Set interval time

--- frontend/src/components/dashboard/components/authCheck.jsx
+++ frontend/src/components/dashboard/components/authCheck.jsx
@@ -8,7 +8,10 @@ const useAuth = () => {
         if (token) {
             const decodedToken = JSON.parse(atob(token.split('.')[1])); // Decoding the token payload
             const tokenExpirationTime = decodedToken.exp * 1000; // Convert expiration time to milliseconds
-            if (Date.now() > tokenExpirationTime) {
+            const currentTime = Date.now();
+            const remainingTime = tokenExpirationTime - currentTime;
+
+            if (currentTime > tokenExpirationTime) {
                 // Token has expired, remove it from local storage
                 localStorage.removeItem('access');
                 setAuthenticated(false);
@@ -22,7 +25,28 @@ const useAuth = () => {
 
     useEffect(() => {
         checkAuthenticated();
-    }, []);
+
+        // Get the token expiration time
+        const token = localStorage.getItem('access');
+        if (token) {
+            const decodedToken = JSON.parse(atob(token.split('.')[1])); // Decoding the token payload
+            const tokenExpirationTime = decodedToken.exp * 1000; // Convert expiration time to milliseconds
+
+            // Calculate the remaining time until the token expiration
+            const currentTime = Date.now();
+            const remainingTime = tokenExpirationTime - currentTime;
+
+            // Run the checkAuthenticated function after the remaining time has passed
+            const interval = setInterval(() => {
+                checkAuthenticated();
+            }, remainingTime);
+
+            // Clean up the interval when the component is unmounted
+            return () => {
+                clearInterval(interval);
+            };
+        }
+    }, [checkAuthenticated]);
 
     return authenticated;
 };
