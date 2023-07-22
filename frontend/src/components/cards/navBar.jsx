c3cf79da1d5ea901bae99a500b5776bc5fa8e32b
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-22 11:10:48 +0100

    Code clean up

--- frontend/src/components/cards/navBar.jsx
+++ frontend/src/components/cards/navBar.jsx
@@ -98,45 +98,6 @@ function NavBar() {
                             </span>
                         </Offcanvas.Body>
                     </Offcanvas>
-                    {/* <div className="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div className="offcanvas-header">
-                            <h5 className="offcanvas-title text-secondary" id="offcanvasNavbarLabel">Charichdeb Sports Solution</h5>
-                            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div className="offcanvas-body">
-                            <div className="navbar-nav justify-content-center flex-grow-1 pe-3" id="navbarNav">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item me-3">
-                                        <NavLink to="/" className='nav-link' aria-current="page">Home</NavLink>
-                                    </li>
-
-                                    <li className="nav-item me-3">
-                                        <NavLink to="/about" className='nav-link' aria-current="page">About</NavLink>
-                                    </li>
-                                    <li className="nav-item me-3">
-                                        <NavLink to="/services" className='nav-link'>Services</NavLink>
-                                    </li>
-                                    <li className="nav-item me-3">
-                                        <NavLink to="/athletes" className='nav-link'>Athletes</NavLink>
-                                    </li>
-                                    <li className="nav-item me-3">
-                                        <NavLink to="/testimonials" className='nav-link'>Testimonials</NavLink>
-                                    </li>
-                                    <li className="nav-item me-3">
-                                        <NavLink to="/contact" className='nav-link'>Contact</NavLink>
-                                    </li>
-                                </ul>
-                                <span className="vstack d-lg-none .d-xl-block mt-3">
-                                    <Link className='btn btn-outline-secondary text-decoration-none mb-3' to="/login">Log In</Link>
-                                    <Link className='btn btn-secondary text-decoration-none' to="/signup">Sign Up</Link>
-                                </span>
-                            </div>
-                            <span className="d-none d-lg-block">
-                                <Link className='btn btn-outline-secondary text-decoration-none me-1' to="/login">Log In</Link>
-                                <Link className='btn btn-secondary text-decoration-none' to="/signup">Sign Up</Link>
-                            </span>
-                        </div>
-                    </div> */}
                 </div>
             </nav>
             <Outlet />
